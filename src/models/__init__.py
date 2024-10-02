fcfcd6eecd3b15d2642c202369c2a41b744877ec
Gordon Liu <gordonliu1106@gmail.com>
2024-10-01 20:17:01 -0500

    style: change style of models package

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -6,4 +6,10 @@ from multi_arcface import MultiArcface
 from spatial_cross_attention import SpatialCrossAttention
 from temporal_self_attention import TemporalSelfAttention
 
-__all__ = ['LightFormer', 'Backbone', 'Decoder', 'Encoder', 'MultiArcface', 'SpatialCrossAttention', 'TemporalSelfAttention']
+__all__ = ['LightFormer',
+           'Backbone',
+           'Decoder',
+           'Encoder',
+           'MultiArcface',
+           'SpatialCrossAttention',
+           'TemporalSelfAttention']
