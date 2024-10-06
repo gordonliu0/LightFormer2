6308dca98a43eca1b5e4fd185249b8ce78e02950
Gordon Liu <gordonliu1106@gmail.com>
2024-10-06 10:36:20 -0500

    chore: Clean up models module

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,7 +2,7 @@ from models.lightformer import LightFormer
 from models.backbone import Backbone
 from models.decoder import Decoder
 from models.encoder import Encoder
-from models.multi_arcface import SubcenterArcface
+from models.subcenter_arcface import SubcenterArcface
 from models.spatial_cross_attention import SpatialCrossAttention
 from models.temporal_self_attention import TemporalSelfAttention
 
