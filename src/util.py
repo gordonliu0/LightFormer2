ee35fc483a492de0c7bd714d07f1cd72ac208b4b
Gordon Liu <gordonliu1106@gmail.com>
2024-10-13 17:18:40 -0500

    feat: Added more animations for the util

--- src/util.py
+++ src/util.py
@@ -1,24 +1,31 @@
 import sys
 import time
 import concurrent.futures
+from enum import Enum
 
-def animated_loading():
-    'Threading Animation'
-    chars = "|/-\\"
+class AnimationSequence(Enum):
+    DEFAULT = "|/-\\"
+    CROSS = "x+"
+    CIRCLE = "◐◓◑◒"
+
+default = "|/-\\"
+
+def animated_loading(chars):
+    'Threading animation. Define the animated spinner using the chars parameter.'
     for char in chars:
-        sys.stdout.write('\r'+'Working...'+char)
+        sys.stdout.write('\r'+'Working '+char)
         time.sleep(.1)
         sys.stdout.flush()
 
 # Run a function with loading animation.
-def run_with_animation(f, args=(), kwargs={}, animation=animated_loading, name='process'):
+def run_with_animation(f, args=(), kwargs={}, animation_chars=AnimationSequence.DEFAULT, name='process'):
     'Animate a function f. '
     with concurrent.futures.ThreadPoolExecutor(max_workers=1, thread_name_prefix=name) as executor:
         print(f"Starting process '{name}'")
         start_time = time.time()
         future = executor.submit(f, *args, **kwargs)
         while not future.done():
-            animated_loading()
+            animated_loading(animation_chars)
         sys.stdout.write('\r' + ' ' * 20 + '\r')
         sys.stdout.flush()
         end_time = time.time()
