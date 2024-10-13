2ce20b6b8d7850d74cac2fa992327104945393bc
Gordon Liu <gordonliu1106@gmail.com>
2024-10-13 15:37:41 -0500

    add timing to run_with_animation util

--- src/util.py
+++ src/util.py
@@ -14,11 +14,16 @@ def animated_loading():
 def run_with_animation(f, args=(), kwargs={}, animation=animated_loading, name='process'):
     'Animate a function f. '
     with concurrent.futures.ThreadPoolExecutor(max_workers=1, thread_name_prefix=name) as executor:
-        print("Starting process", name)
+        print(f"Starting process '{name}'")
+        start_time = time.time()
         future = executor.submit(f, *args, **kwargs)
         while not future.done():
             animated_loading()
         sys.stdout.write('\r' + ' ' * 20 + '\r')
         sys.stdout.flush()
-        print("Finished process", name)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        hours, rem = divmod(elapsed_time, 3600)
+        minutes, seconds = divmod(rem, 60)
+        print(f"Finished process '{name}' in {int(hours):02d}h:{int(minutes):02d}m:{seconds:05.2f}s.")
         return future.result()
